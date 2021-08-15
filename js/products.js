ab7de6567d86c5ca65d720edcc4b99314043a669
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2021-08-15 16:57:19 -0300

    Lista de productos
    Intento n°: 1.

--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
 
+//Funcion para listar los productos
+function listaProductos(){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < currentCategoriesArray.length; i++){
+        let category = currentCategoriesArray[i];
+
+        if (((minCount == undefined) || (minCount != undefined && parseInt(category.productCount) >= minCount)) &&
+            ((maxCount == undefined) || (maxCount != undefined && parseInt(category.productCount) <= maxCount))){
+
+            htmlContentToAppend += `
+            <a href="product-info.html" class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="` + product.images[1] + `" alt="` + product.description + `" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">`+ product.name +`</h4>
+                            <small class="text-muted">Ya se vendieron ` + category.soldCount + ` iguales.</small>
+                        </div>
+                        <p class="mb-1">` +product.description + `</p>
+                    </div>
+                </div>
+            </a>
+            `
+        }
+
+        document.getElementById("container p-5").innerHTML = htmlContentToAppend;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+        //Obtengo la info y la despliego <=> todo está bien
+        getJSONData(PRODUCTS_URL).then(function(resultObj){
+                if(resultObj.status === "ok"){
+                    //Muestro los productos
+                    listaProductos(resultObj.data);
+                }
+        });
 });
