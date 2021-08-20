4b7543b812642db083e4352246d5deacee34811b
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2021-08-19 22:22:02 -0300

    Login terminado

--- js/login.js
+++ js/login.js
@@ -2,4 +2,18 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
+//Funcion que luego voy a usar para validar datos, mientras la utilizo para redirigir
+function validacion(){
+    let user = document.getElementById("usuario").value;
+    let password = document.getElementById("contraseña").value;
+    sessionStorage.setItem("usuario", user);
+    sessionStorage.setItem("contraseña", password);
+    window.location.href = "index.html";
+}
+
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("boton-ingresar").addEventListener("click", function(event){
+        validacion();
+    })
+})
 
