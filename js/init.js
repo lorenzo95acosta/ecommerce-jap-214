0e7e1a445e502e5f89f374ce0caaefd16b817567
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2021-08-31 22:16:02 -0300

    Muestro el Usuario en el nav

--- js/init.js
+++ js/init.js
@@ -44,9 +44,16 @@ var getJSONData = function(url){
     });
 }
 
+function mostrarUsuario(){
+let nombre = sessionStorage.getItem('usuario');
+let usuario = document.getElementById("info_usuario");
+usuario.innerHTML = nombre;
+
+}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+  mostrarUsuario();
 });
