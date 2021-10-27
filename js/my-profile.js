a4f8438815b406acb4ce21c23345c55982c4c219
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2021-10-27 17:19:32 -0300

    Cambios formulario

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,26 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+//Funcion de Bootsrap para las validaciones
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
+
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
