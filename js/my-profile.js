b9df2e9906295683056edfcd33e4f8707706302f
lorenzo95acosta <lorenzo.a.gavacciuto@gmail.com>
2021-10-30 14:12:53 -0300

    Cambios imagen

--- js/my-profile.js
+++ js/my-profile.js
@@ -70,9 +70,22 @@ function mostrarDatos(){
     let contenedor = document.getElementById("info-usuario");
     contenedor.innerHTML = contenido;
   }
-
 }
 
+function previewFile(){
+  let preview = document.getElementById('foto-exhibida');
+  let file = document.querySelector('input[type=file]').files[0]
+  let reader = new FileReader();
+  if(file){
+    reader.readAsDataURL(file);
+  } else{
+    preview.src = "img/usr.png"; 
+  }
+  reader.onloadend = ()=>{
+    preview.src = reader.result;
+    console.log(reader.result)
+  }
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
   mostrarDatos();
